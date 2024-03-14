df814faeda2c7bf7a506cf19dd28c32720d6729d
AmiSamarakoon <s224318859@deakin.edu.au>
2024-03-14 12:17:11 +1100

    add new functions subtraction, multiplication, division

--- server.js
+++ server.js
@@ -17,6 +17,33 @@ app.get('/addTwoNumbers',(req,res)=>{
 
 })
 
+app.get('/substractTwoNumbers',(req,res)=>{
+    let value1 = req.query.num1;
+    let value2 = req.query.num2;
+    let result =  parseInt(value1) - parseInt(value2);
+    let response = {data: result, statusCode: 200, message: 'successful'};
+    res.json(response);
+
+})
+
+app.get('/multiplyTwoNumbers',(req,res)=>{
+    let value1 = req.query.num1;
+    let value2 = req.query.num2;
+    let result =  parseInt(value1) * parseInt(value2);
+    let response = {data: result, statusCode: 200, message: 'successful'};
+    res.json(response);
+
+})
+
+app.get('/divideTwoNumbers',(req,res)=>{
+    let value1 = req.query.num1;
+    let value2 = req.query.num2;
+    let result =  parseInt(value1) / parseInt(value2);
+    let response = {data: result, statusCode: 200, message: 'successful'};
+    res.json(response);
+
+})
+
 app.listen(port, () => {
     console.log("App listening to: " + port)
 })
